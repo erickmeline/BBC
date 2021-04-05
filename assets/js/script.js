e24a8896268ee44e581d0f00b80632a0fad54e6d
Erick Meline <erickmeline@gmail.com>
2021-04-05 08:58:52 -0700

    get current data api working

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,20 +2,19 @@
  * Global constants
  */
 const url_current = 'https://api.openweathermap.org/data/2.5/weather';
+const url_uvIndex = 'http://api.openweathermap.org/data/2.5/uvi';
 const url_forcast = 'https://api.openweathermap.org/data/2.5/forecast';
 const api_kei = '7875554f2197f6cdb5c5b47cbfb1c260';
-const search = document.querySelector('button');
+const searchEl = document.querySelector('button');
+const currentEl = document.querySelector('.current');
 
 /**
  * Fetch request
  */
 const fetchData = (url) => {
-    fetch(url).then((response) => {
+    return fetch(url).then((response) => {
         if (response.ok) {
-            response.json().then((data) => {
-                console.log('data:',data);
-                return data;
-            })
+            return response.json();
         }
         else {
             console.error('Error:', response.statusText);
@@ -23,6 +22,39 @@ const fetchData = (url) => {
     }).catch((error) => {
         console.error('Error: no response');
     });
+}
+
+/**
+ *  Kick off a current weather request
+ */
+const displayCurrent = () => {
+    let location = document.querySelector('[type="search"').value;
+    if (location) {
+        const currentUrl = `${url_current}?q=${location}&units=imperial&appid=${api_kei}`;
+        fetchData(currentUrl).then((data) => {//console.log('data:',data);
+            const headline = document.createElement('h2');
+            headline.textContent = `${data.name} ${moment().format('dddd, MMMM Do')}`;
+            currentEl.appendChild(headline);
+            const dataP1 = document.createElement('p');
+            dataP1.textContent = `Temperature: ${data.main.temp} °F`;
+            currentEl.appendChild(dataP1);
+            const dataP2 = document.createElement('p');
+            dataP2.textContent = `Humidity: ${data.main.humidity}%`;
+            currentEl.appendChild(dataP2);
+            const dataP3 = document.createElement('p');
+            dataP3.textContent = `Wind Speed: ${data.wind.speed} MPH`;
+            currentEl.appendChild(dataP3);
+            const uvUrl = `${url_uvIndex}?lat=${data.coord.lat}&lon=${data.coord.lon}&appid=${api_kei}`;
+            fetchData(uvUrl).then((data) => {//console.log('data:',data);
+                const dataP4 = document.createElement('p');
+                dataP4.innerHTML = `UV Index: <span>${data.value}</span>`;
+                currentEl.appendChild(dataP4);
+            });
+
+        });
+        // localStorage.setItem('locations', JSON.stringify(locations));
+
+    }
 }
 
 /**
@@ -39,23 +71,11 @@ const getLocation = () => {
     }
 }
 
-/**
- *  Save to local store
- */
-const handleSearch = () => {
-    let location = document.querySelector('[type="search"').value;
-    if (location) {
-        const url = `${url_current}?q=${location}&appid=${api_kei}`;
-        const data = fetchData(url).then((data) => {console.log(data)});
-        // console.log(data);
-        // localStorage.setItem('locations', JSON.stringify(locations));
 
-    }
-}
 
 
 
 /**
  *  Add listeners
  */
-search.addEventListener('click', handleSearch);
+searchEl.addEventListener('click', displayCurrent);
