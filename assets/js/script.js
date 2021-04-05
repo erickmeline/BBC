26d0d52628dcd29f5955e3029be5758b7fe2a600
Erick Meline <erickmeline@gmail.com>
2021-04-04 19:49:00 -0700

    get geolocation ...maybe

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+// globals
 const url_current = 'api.openweathermap.org/data/2.5/weather';
 const url_forcast = 'api.openweathermap.org/data/2.5/forecast';
 const api_kei = '7875554f2197f6cdb5c5b47cbfb1c260';
@@ -18,3 +19,15 @@ const fetchData = (url) => {
         console.error('Error: no response');
     });
 }
+
+const getLocation = () => {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((position) => {
+            return position;
+        });
+    } else {
+        alert('Error: Unable access current location');
+    }
+}
+
+console.log('position',getLocation());
