d9cd467d86cb423774516d79d6c77c47ef2f6c8a
Erick Meline <erickmeline@gmail.com>
2021-04-04 19:36:44 -0700

    add openweather

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+const url_current = 'api.openweathermap.org/data/2.5/weather';
+const url_forcast = 'api.openweathermap.org/data/2.5/forecast';
+const api_kei = '7875554f2197f6cdb5c5b47cbfb1c260';
+
+// fetch data
+const fetchData = (url) => {
+    fetch(url).then((response) => {
+        if ( response.ok ) {
+            response.json().then((data) => {
+                console.log('data:',data);
+                return data;
+            })
+        }
+        else {
+            console.error('Error:', response.statusText);
+        }
+    }).catch((error) => {
+        console.error('Error: no response');
+    });
+}
